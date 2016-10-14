8154951afd258e94fac4b6379e0d773e9896387d
Tomoya Tabuchi <t@tomoyat1.com>
2016-10-14 16:49:44 +0900

    Add version output

--- src/srv.c
+++ src/srv.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "man_bomber_config.h"
+
 
 int main(int argc, char **argv)
 {
+	printf("Start $ man bomber server version %d.%d\n",
+	    man_bomber_VERSION_MAJOR,
+	    man_bomber_VERSION_MINOR);
+	sleep(1);
 	execlp("man", "man", "man", NULL);
 	exit(0);
 }
