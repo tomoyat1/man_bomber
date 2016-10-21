ab89eb9564b547e71360e45c19fbfb979b47ca04
Tomoya Tabuchi <t@tomoyat1.com>
2016-10-21 19:35:18 +0900

    Add struct metadata
    * Add struct metadata.
    * Add comments on server-client message format.
    * Add include guards.

--- src/include/man_bomber.h
+++ src/include/man_bomber.h
@@ -1,42 +1,87 @@
+#ifndef _MAN_BOMBER_H_
+#define _MAN_BOMBER_H_
+
+#include <stddef.h>
+
+/*
+ * +----------------------------+
+ * |Server-client message format|
+ * +----------------------------+
+ * Server-client messaging will be in the following format
+ * -----------------------------------------------------------------------------
+ *           player_offset         player_cnt
+ * struct metadata | struct player ...... struct player
+ *             bomb_offset         bomb_cnt
+ *                 | struct bomb   ...... struct bomb
+ *             wall_offset         wall_cnt
+ *                 | struct wall   ...... struct wall
+ * -----------------------------------------------------------------------------
+ * where offsets and cnts are struct metadata fields.
+ */
+
+struct metadata {
+	/* Client id */
+	long int id;
+
+	/*
+	 * Tick count from beginning of round.
+	 * srv->cli: contains server tick count. Clients should sync their
+	 *           tick count with this value.
+	 * cli->srv: contains client tick count. Server will gracefully
+	 *           ignore all messages with a tick count which does not
+	 *           match that of the server.
+	 */
+	int tick;
+
+	/* Offset of player array from front of message in bytes. */
+	size_t player_offset;
+
+	/* Count of player structs. */
+	size_t player_cnt;
+
+	/* Offset of player array from front of message. */
+	size_t bomb_offset;
+
+	/* Count of bomb structs. */
+	size_t bomb_cnt;
+
+	/* Offset of player array from front of message. */
+	size_t wall_offset;
+
+	/* Count of wall structs. */
+	size_t wall_cnt;
+};
+
 struct bomb {
 	int id;
 	int x;
 	int y;
 	int vx;
 	int vy;
-	int time; /* in game ticks */
-	int aoe; /* area of effect */
-	int is_alive; /* 0: dead, 1: alive */
+
+	/* in game ticks */
+	int timer;
+
+	/* area of effect */
+	int aoe; 
+
+	/* 0: dead, 1: alive */
+	int is_alive; 
 };
 
 struct player {
 	int x;
 	int y;
+	int bombs;
 };
 
 /* Destructable wall */
 struct wall {
 	int x;
 	int y;
-	int is_alive; /* 0: dead, 1: alive */
+
+	/* 0: dead, 1: alive */
+	int is_alive; 
 };
 
-struct client_data {
-	unsigned int id;
-	struct player p;
-	/* Variable lenght array of struct bombs */
-	struct *bomb bombs;
-	/* Current tick. Ignore data if not in sync with server. */
-	unsigned int cur_tick;
-};
-
-struct server_data {
-	unsigned int id;
-	struct player p;
-	/* Variable length array of struct bombs */
-	struct bomb *bombs;
-	/* Variable length array of struct walls */
-	struct wall *walls;
-	/* Current tick. Ignore data if not in sync with server. */
-	unsigned int cur_tick;
-};
+#endif /* _MAN_BOMBER_H_ */
