3a720c9fe203da9dbc4d0736b1c1ea4a18c67d65
Maeda Yuya <6yu.disney22xx@gmail.com>
2016-10-27 08:45:18 +0900

    Merge master

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -20,10 +20,13 @@
 
 #define ERROR -1
 
+void connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
+                 int scklen, struct hostent *hp);
+
 main(int argc, char *argv[])
 {
 	struct metadata *data;
-	struct player *pla;
+	struct player *pla[4];
 	struct bomb *bo;
 	struct wall *wa;
 	
@@ -36,14 +39,18 @@ main(int argc, char *argv[])
 	
 	int finish;
 
+	/*First Connect*/
+	int connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
+                 int scklen, struct hostent *hp)
+	//data->id = -1;
+	//
+
 	init();
 	while(1){
-
 		if(argc != 3){
 			fprintf(stderr, "Server IPaddress server_port\n");
 			exit(1);
 		}
-
 		/*Socket Make*/
 		if((sd = socket(AF_INET, SOCK_STREAM, 0)) == ERROR){
 			perror("client:socket");
@@ -84,4 +91,44 @@ main(int argc, char *argv[])
 
 	/* finish sign */
 	
+}
+
+void connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
+		 int scklen, struct hostent *hp)
+{
+  if(argc != 3){
+        fprintf(stderr, "Server IPaddress server_port\n");
+        exit(1);
+        }
+
+        /*Socket Make*/
+        if((sd = socket(AF_INET, SOCK_STREAM, 0)) == ERROR){
+        perror("client:socket");
+        exit(1);
+        }
+
+        /* ClientProcess SocketAdress */
+        bzero((char *)&client, sizeof(client));
+        client.sin_family = AF_INET;
+        client.sin_addr.s_addr = inet_addr("127.0.0.1 ");
+        client.sin_port = htons(12345);
+        scklen = sizeof(client);
+
+        /* ServerProcess SocketAdress */
+        bzero((char *)&server, sizeof(server));
+        server.sin_family = AF_INET;
+        if((hp = gethostbyname(argv[1])) == NULL){
+        fprintf(stderr, "ERROR: %s unknown host. \n", argv[1]);
+        exit(1);
+        }
+
+        /*IP Adress*/
+        bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
+	server.sin_port = htons(atoi(argv[2]));
+        scklen =sizeof(server);
+
+        if(connect(sd, (struct sockaddr *)&server,scklen) == ERROR){
+        perror("client:connect");
+        exit(1);
+        }
 }
