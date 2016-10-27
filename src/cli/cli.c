7708fbc9fcd494f508e31534d8987f0906c5a00b
Maeda Yuya <6yu.disney22xx@gmail.com>
2016-10-27 12:58:45 +0900

    temporary

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -20,13 +20,12 @@
 
 #define ERROR -1
 
-void connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
-                 int scklen, struct hostent *hp);
+void make_cnt(int sd, struct sockaddr_in server, struct sockaddr_in client, int scklen, struct hostent *hp, int argc, char *argv[]);
+	
+void main(int argc, char *argv[]){
 
-main(int argc, char *argv[])
-{
 	struct metadata *data;
-	struct player *pla[4];
+	struct player pla[4];
 	struct bomb *bo;
 	struct wall *wa;
 	
@@ -38,54 +37,23 @@ main(int argc, char *argv[])
 	struct hostent *hp;
 	
 	int finish;
-
-	/*First Connect*/
-	int connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
-                 int scklen, struct hostent *hp)
-	//data->id = -1;
-	//
-
+	static int cli_id;
+	
+	/*First Connect  */
+	/*make_cnt(sd,  server, client, scklen, hp, argc, argv);
+	data->id = -1;
+	data_send(sd, data, &pla, &bo,&wa);
+	data_recv(sd, data, &pla, &bo,&wa);*/	
+	//cli_id = data->id;
+	//data->  
+	data->id = atoi(argv[3]);
 	init();
 	while(1){
-		if(argc != 3){
-			fprintf(stderr, "Server IPaddress server_port\n");
-			exit(1);
-		}
-		/*Socket Make*/
-		if((sd = socket(AF_INET, SOCK_STREAM, 0)) == ERROR){
-			perror("client:socket");
-			exit(1);
-		}
 
-		/* ClientProcess SocketAdress */
-		bzero((char *)&client, sizeof(client));
-		client.sin_family = AF_INET;
-		client.sin_addr.s_addr = inet_addr("127.0.0.1");
-		client.sin_port = htons(12345);
-		scklen = sizeof(client);
-
-		/* SeverProcess SocketAdress */
-		bzero((char *)&server, sizeof(server));
-		server.sin_family = AF_INET;
-		if((hp = gethostbyname(argv[1])) == NULL){
-			fprintf(stderr, "ERROR: %s unknown host. \n", argv[1]);
-			exit(1);
-		}
-
-		/*IP Adress*/
-		bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
-		server.sin_port = htons(atoi(argv[2]));
-		scklen =sizeof(server);
-
-		if(connect(sd, (struct sockaddr *)&server,scklen) == ERROR){
-			perror("client:connect");
-			exit(1);
-		}
-
-		data_send(sd, data, &pla, &bo, &wa);
-		data_recv(sd, data, *pla, *bo, *wa);
-		finish = refreshAll(data, pla, bo, wa);
-
+		make_cnt(sd,  server, client, scklen, hp, argc, argv);
+		data_send(sd, data, &pla, &bo,&wa);
+		data_recv(sd, data, &pla, &bo,&wa); 
+ 		finish = refreshAll(data, bo, &pla[0], wa);
 		if(finish == 1) break;
 	}
 
@@ -93,37 +61,37 @@ main(int argc, char *argv[])
 	
 }
 
-void connect(int sd, struct sockaddr_in server, struct sockaddr_in client,
-		 int scklen, struct hostent *hp)
+void make_cnt(int sd, struct sockaddr_in server, struct sockaddr_in client,
+		 int scklen, struct hostent *hp, int argc, char *argv[])
 {
   if(argc != 3){
         fprintf(stderr, "Server IPaddress server_port\n");
         exit(1);
         }
 
-        /*Socket Make*/
+        //Socket Make
         if((sd = socket(AF_INET, SOCK_STREAM, 0)) == ERROR){
         perror("client:socket");
         exit(1);
         }
 
-        /* ClientProcess SocketAdress */
+	//ClientProcess SocketAdress 
         bzero((char *)&client, sizeof(client));
         client.sin_family = AF_INET;
         client.sin_addr.s_addr = inet_addr("127.0.0.1 ");
         client.sin_port = htons(12345);
         scklen = sizeof(client);
 
-        /* ServerProcess SocketAdress */
+        //ServerProcess SocketAdress
         bzero((char *)&server, sizeof(server));
         server.sin_family = AF_INET;
-        if((hp = gethostbyname(argv[1])) == NULL){
+       /* if((hp = gethostbyname(argv[1])) == NULL){
         fprintf(stderr, "ERROR: %s unknown host. \n", argv[1]);
         exit(1);
         }
-
-        /*IP Adress*/
-        bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
+        //IP Adress
+        bcopy(hp->h_addr[0], &server.sin_addr, hp->h_length);*/
+	server.sin_addr.s_addr = inet_addr(argv[1]);
 	server.sin_port = htons(atoi(argv[2]));
         scklen =sizeof(server);
 
